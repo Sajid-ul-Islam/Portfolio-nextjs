0a55885222afda064f4ddc3e3b390473aab6812b
saajiidi <saajiidi@gmail.com>
2026-06-04 17:39:01 +0600

    .

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'via.placeholder.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+      },
     ],
   },
 };
