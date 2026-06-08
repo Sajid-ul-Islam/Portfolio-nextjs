64c1d57b294bfd28647988e7e8bfa2fb5de654fc
Bearded Bengali <nsuer.sajid@gmail.com>
2026-06-08 07:16:08 +0600

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${tiroBangla.variable}`}>
+    <html lang="en" className={`${tiroBangla.variable}`} suppressHydrationWarning>
       <head>
         <style dangerouslySetInnerHTML={{
           __html: `
